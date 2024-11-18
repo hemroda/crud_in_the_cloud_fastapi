4c49f7a7ec26e6755e343facfb6e47ef746e6f75
hemroda <hemroda-dev@pm.me>
2024-11-18 21:14:43 +0100

    rebuild css, avoid including node_modules files in tailwind contents

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
         './templates/**/*.html', // Adjust to your HTML template folder
         './**/*.html',          // If you have standalone HTML files
         './static/src/**/*.css', // If you use Tailwind in raw CSS files
-        './**/*.js',            // For JavaScript files
+        './static/js/*.js',            // For JavaScript files
         './**/*.py',            // If Tailwind classes are in Python templates
       ],
   theme: {
