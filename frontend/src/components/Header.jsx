5fc0b67e6bac11a6d2b80de9da90484c54ce4b47
hemroda <hemroda-dev@pm.me>
2024-11-22 12:35:57 +0100

    frontend: add favicons

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="flex justify-between items-center w-full max-w-5xl">
-      <h1 className="text-3xl font-bold">Crud@Cloud</h1>
+      <h1 className="text-2xl font-light text-gray-900d">Crud@Cloud</h1>
       <nav>
         <ul>
           <li><a href="/api/users/">Users</a></li>
